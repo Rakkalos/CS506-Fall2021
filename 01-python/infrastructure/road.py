7ad45da46c63eed3b9a5fe980cf24493278a561d
Adrien Boulet <adriboulet97@gmail.com>
2021-09-23 14:40:08 -0400

    edited road.py : cute little road populated with nj driver

--- 01-python/infrastructure/road.py
+++ 01-python/infrastructure/road.py
@@ -1,3 +1,13 @@
 def draw_road():
-    print("road not found")
+    #print("road not found")
+    print()
+    print("\n")
+    print("********************************")
+    print("  ______")
+    print("__|__|__|__")
+    print("|____|_____|    cute little road")
+    print("  ()    ()")
+    print("********************************")
+    print("\n")
+    print()
     return
